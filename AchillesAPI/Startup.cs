14e4ef3283d4f9bf297f595d6386dfe6884ca45d
Gmandam <Gmandam@mykolab.com>
2018-02-25 21:02:24 +0000

    API Connection String Adjustment

--- AchillesAPI/Startup.cs
+++ AchillesAPI/Startup.cs
@@ -16,12 +16,24 @@ namespace AchillesAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public ILoggerFactory LoggerFactory { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            LoggerFactory = loggerFactory;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
