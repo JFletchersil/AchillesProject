945567a3694507ce63d61ef895c9841751e2a2d4
Gmandam <lukejunk1234@gmail.com>
2018-03-13 23:18:49 +0000

    API Work - Updated API to make retrieval possible from database - Updated models to match DB configuration - Created Appropriate Models to retrieve Exercise Type and Exercises - Created API Call to get all Exercise Types - Updated Front End to do promise for data - Adjusted Front end to correctly account for Exercise types - Fully formalised API style and configuration

--- AchillesAPI/Contexts/AngularDbContext.cs
+++ AchillesAPI/Contexts/AngularDbContext.cs
@@ -1,18 +1,19 @@
 ﻿using AchillesAPI.Models.DbModels;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AchillesAPI.Contexts
 {
-    public class AngularDbContext : DbContext
+    public partial class AngularDbContext : DbContext
     {
         public AngularDbContext(DbContextOptions options) : base(options)
         {
-
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ExerciseType>().HasMany(et => et.Exercises).WithOne(x => x.ExerciseType);
+        }
+
         public DbSet<Stage> Stages { get; set; }
         public DbSet<Exercise> Exercises { get; set; }
         public DbSet<ExerciseType> ExerciseTypes { get; set; }
--- AchillesAPI/Models/DbModels/Exercise.cs
+++ AchillesAPI/Models/DbModels/Exercise.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -11,10 +12,12 @@ namespace AchillesAPI.Models.DbModels
         public Guid ExerciseID { get; set; }
         public string ExerciseName { get; set; }
         public string VideoLink { get; set; }
-        public ExerciseType ExerciseType { get; set; }
-        // Three below need to be changed in DB
-        public double Reps { get; set; }
-        public double Sets { get; set; }
-        public double Time { get; set; }
+        public Guid ExerciseTypeID { get; set; }
+        [ForeignKey("ExerciseTypeID")]
+        [Required]
+        public virtual ExerciseType ExerciseType { get; set; }
+        public double? Reps { get; set; }
+        public double? Sets { get; set; }
+        public double? Time { get; set; }
     }
 }
